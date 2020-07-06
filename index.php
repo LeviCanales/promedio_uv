9c1e915365930b9f861c3b6c56230331701a129e
LeviCanales <milevi271@gmail.com>
2020-07-06 14:56:24 -0600

    Cambio Titulo

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<?php
+	$titulo = 'Promedio Uv'
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Promedio Uv</title>
+	<title><?php echo $titulo; ?></title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
